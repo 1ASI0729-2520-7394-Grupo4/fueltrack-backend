3d8c64e8c2a3df391611baad27a4378528f068bb
GiorgioAwad <170798404+GiorgioAwad@users.noreply.github.com>
2025-11-01 17:57:15 -0500

    feat: New backend repository structure

---
+++ src/main/java/com/example/fuel/track/backend/FueltrackBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.fuel.track.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FueltrackBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FueltrackBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fuel/track/backend/FueltrackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fuel.track.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FueltrackBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
