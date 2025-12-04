32e861c29a9e0fa7546610cc7c5d70e0679aac86
JulyP <u20211d760@upc.edu.pe>
2025-12-04 00:09:34 -0500

    feat: add valueobjects.

---
+++ src/main/java/com/example/fuel/track/backend/iam/domain/model/valueobjects/Roles.java
@@ -0,0 +1,14 @@
+package com.example.fuel.track.backend.iam.domain.model.valueobjects;
+
+/**
+ * Roles
+ * <p>
+ *     This enum represents the roles in the system.
+ * </p>
+ */
+public enum Roles {
+    ROLE_USER,
+    ROLE_SOLICITANTE,
+    ROLE_PROVEEDOR,
+
+}
