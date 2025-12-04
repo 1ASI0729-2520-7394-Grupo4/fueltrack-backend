2db25963d7e28276754fbe1aef93661a833fd598
JulyP <u20211d760@upc.edu.pe>
2025-12-04 00:08:02 -0500

    feat: add sign in command.

---
+++ src/main/java/com/example/fuel/track/backend/iam/domain/model/commands/SignInCommand.java
@@ -0,0 +1,16 @@
+package com.example.fuel.track.backend.iam.domain.model.commands;
+
+
+
+/**
+ * Sign in command
+ * <p>
+ *     This class represents the command to sign in a user.
+ * </p>
+ * @param username the username of the user
+ * @param password the password of the user
+ *
+ *
+ */
+public record SignInCommand(String username, String password) {
+}
